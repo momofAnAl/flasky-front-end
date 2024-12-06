45aa3e10286205a243b765552415946396d7b92a
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-12-06 13:07:43 -0600

    Add components directory, create Cat and CatList components

---
+++ src/components/Cat.jsx
@@ -0,0 +1,11 @@
+const Cat = () => {
+    return(
+        <li>
+            <h3>Name of Cat</h3>
+            <p>🐈</p>
+            <button>Pet Cat</button>
+        </li>
+    );
+};
+
+export default Cat;
