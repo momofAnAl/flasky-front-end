733e7361622c4a5c65ce4ab730da4dbc1c80871f
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-12-10 14:43:06 -0500

    React: Props Livecode changes

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -1,11 +1,23 @@
-const Cat = () => {
+import PropTypes from 'prop-types';
+
+const Cat = ({ name, color, personality, caretaker }) => {
     return(
         <li>
-            <h3>Name of Cat</h3>
+            <h3>Name: {name}</h3>
+            <h3>Color: {color}</h3>
+            <h3>Personality: {personality}</h3>
+            <h3>Caretaker: {caretaker}</h3>
             <p>🐈</p>
             <button>Pet Cat</button>
         </li>
     );
+};
+
+Cat.propTypes = {
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    personality: PropTypes.string.isRequired,
+    caretaker: PropTypes.string.isRequired
 };
 
 export default Cat;
